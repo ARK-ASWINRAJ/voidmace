52dd45b90840b39d6ad7d24d9dc9b8853cbad87a
Aswinraj <aswinrajkailath@gmail.com>
2020-09-12 18:25:46 +0530

    added nickname feature

--- index.js
+++ index.js
@@ -23,31 +23,34 @@ app.get('/general', (req, res) => {
     res.sendFile(__dirname + '/public/general.html');
 });
 
+app.get('/enter', (req, res) => {
+    res.sendFile(__dirname + '/public/enter.html');
+});
+
 // tech namespace
 const tech = io.of('/tech');
-
 var online= {general:0, technical:0, confessions:0};
 
 tech.on('connection', (socket) => {
     var currentRoomId;
+    var currentNickname
     socket.on('join', (data) => {
         socket.join(data.room);
         currentRoomId = data.room;
+        currentNickname=data.nickname;
         online[currentRoomId]=online[currentRoomId]+1;
-        tech.in(data.room).emit('userjl', `New user joined ${data.room} room!.   Online:${online[currentRoomId]}`);
+        tech.in(data.room).emit('userjl', `${data.nickname}  joined  ${data.room} room!      ~ Online:${online[currentRoomId]}`);
     })
 
     socket.on('message', (data) => {
-        
-        tech.in(data.room).emit('message', data.msg);
+        let fullmsg= data.nickname+": "+ data.msg
+        tech.in(data.room).emit('message', fullmsg);
     });
 
    socket.on('disconnect', () => {
-       
-        online[currentRoomId]=online[currentRoomId]-1;
-        tech.in(currentRoomId).emit('userjleft',  `User left the room!.   Online:${online[currentRoomId]}` );
-          
-    })
-    
-})
 
+        online[currentRoomId]=online[currentRoomId]-1;
+        tech.in(currentRoomId).emit('userjleft',` ${currentNickname}  left the room!.     ~ Online:${online[currentRoomId]}` );
+    })
+
+})
